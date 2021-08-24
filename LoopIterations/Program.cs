66b59ba7afe0f8b711736a7889c54ef0b352caae
Sean Morgan <seanmorgan@Seans-Air.attlocal.net>
2021-08-24 11:57:31 -0500

    having fun with loops

---
+++ LoopIterations/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LoopIterations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int x = 0; x < 15; x++)
+            {
+                for(int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine($"{i}\t{i}\t{i}\t{i}\t{i}");
+                }
+            }
+            /*Is this the most matrix thing you ever seen.
+             * To perform the marix just change the outer
+             * loop variable to the inner loop variable and
+             * delete the inner int data type.*/
+
+        }
+    }
+}
